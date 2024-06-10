63de95cc25c63fe4f033b78e43ccea64025c3d34
Emily Yao <emyao@cisco.com>
2024-06-10 11:58:15 -0400

    project euler init

---
+++ project euler/002.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int res = 0;
+    int p2 = 0;
+    int p1 = 1;
+
+    while(p2 + p1 < 4000000) {
+        int tmp = p2 + p1;
+        p1 = p2;
+        p2 = tmp;
+        if(tmp % 2 == 0) {
+            res += tmp;
+        }
+    }
+
+    cout << res << endl;
+}
